a4d2a9369981dd51390c9df39e5dc583ab00397d
Serhat Ozdilek <132130050@ogr.uludag.edu.tr>
2025-01-22 00:09:20 +0300

    kodlar commit edildi.

---
+++ çekiliş projesi/kodlar/CekilisKuraOtomasyonu/CekilisKuraOtomasyonu/obj/Debug/net6.0/CekilisKuraOtomasyonu.RazorAssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System;
+using System.Reflection;
+
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ProvideApplicationPartFactoryAttribute(("Microsoft.AspNetCore.Mvc.ApplicationParts.ConsolidatedAssemblyApplicationPartFact" +
+    "ory, Microsoft.AspNetCore.Mvc.Razor"))]
